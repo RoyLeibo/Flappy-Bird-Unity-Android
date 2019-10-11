c8432568a0ee15fe3ae2d0a0dcf0c1726728a62a
Fatuity <danial0201@walla.com>
2019-10-11 19:36:49 +0300

    commit

--- Assets/Scripts/BoostPool.cs
+++ Assets/Scripts/BoostPool.cs
@@ -6,25 +6,40 @@ using UnityEngine;
 public class BoostPool : MonoBehaviour
 {
     //TODO: continue naming variables and choosing values.
-    public float spawnRate = 5f;
+    public float spawnRate = 4f;
     public int poolSize = 5;
-    public float columnYMin = -1.1f;
-    public float columnYMax = 0.9f;
+    public float boostYMin = -1.1f;
+    public float boostYMax = 0.9f;
 
     private float timeSinceLastSpawn = 0;
     private float spawnXPos = 7f;
-    private int currentColumn = 0;
+    private int currentBoost = 0;
     public GameObject[] boosts;
     private GameObject[] boostsPool;
+    private Vector2 objPoolPos = new Vector2(-15f,-25f);
+    System.Random rand = new System.Random();
     // Start is called before the first frame update
     void Start()
     {
-        
+        boostsPool = new GameObject[poolSize];
+        for(int i=0;i<poolSize;i++){
+            int whichBoost = rand.Next(0,boosts.Length);
+            boostsPool[i] = (GameObject)Instantiate(boosts[whichBoost],objPoolPos,Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastSpawn += Time.deltaTime;
+        if(!GameControl.Instance.isGameOver && timeSinceLastSpawn >= spawnRate){
+            timeSinceLastSpawn = 0;
+            float spawnYPos = Random.Range(boostYMin,boostYMax);
+            float xRange = spawnRate*1.5f - 1f; // spawnRate *1.5 / 2 - boost size.
+            spawnXPos = spawnXPos + Random.Range(1,xRange);
+            boostsPool[currentBoost].transform.position = new Vector2(spawnXPos,spawnYPos);
+            currentBoost++;
+            if(currentBoost>=poolSize){currentBoost = 0;}
+        }
     }
 }
