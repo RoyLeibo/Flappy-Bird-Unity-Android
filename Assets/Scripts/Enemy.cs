db1fb372840081f56a77838ec1b3185e3e38ce9e
Fatuity <danial0201@walla.com>
2019-10-15 14:34:43 +0300

    fireball and enemy are working
    fireball is being created by charizard's mouth, and has speed so it seems like it being shot.
    the fireball is being destroyed when it hit a column or an enemy.
    there is an enemyPool that is constantly using the former enemies.
    created zapdos animation.
    when an enemy hits charizard, charizard is dead and when it is getting shot it is dead and the score is updated.
    did some fmodifications in boost script, and added shot function to player.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private Vector3 force = new Vector3(0,0.2f,0);
+    private float moveRate = 0.1f;
+    private float currentMoveTime = 0;
+    public bool isDead = false;
+    private float maxYPos =2.5f;
+    private float minYPos = -2f; 
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if(GameControl.Instance.isGameOver){
+            force = Vector3.zero;
+        }
+        currentMoveTime += Time.deltaTime;
+        if(!isDead){
+            if(transform.position.y >= maxYPos || transform.position.y <= minYPos){
+                force = -force;
+            }
+            if(currentMoveTime>=moveRate){
+                currentMoveTime = 0;
+                AddForce();
+                anim.SetTrigger("Fly");
+            }
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "FireBall"){
+            //"kill" the enemy
+            isDead = true;
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+    private void AddForce(){
+        transform.position += force;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
 {
     public float upForce = 200f;
     public bool isDead = false;
+    public float shootRate = 2f;
+    public float xFB = 1.35f;
+    public float yFB = -0.4f;
+    public GameObject fireball;
+    private float timeSinceLastShot = 0;
     private Rigidbody2D rb2d;
     private Animator animator;
     // Start is called before the first frame update
@@ -18,11 +23,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeSinceLastShot += Time.deltaTime;
         if(isDead) return;
         if(Input.GetMouseButtonDown(0)){
             rb2d.velocity = Vector2.zero;
             rb2d.AddForce(new Vector2(0,upForce));
             animator.SetTrigger("Fly");
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            OnShoot();
         }
     }
     private void OnCollisionEnter2D(Collision2D other) {
@@ -34,5 +43,10 @@ public class Player : MonoBehaviour
     //activate when shooting key pressed.
     private void OnShoot(){
         //TODO: make the fireball appear with certain shootRate.
+        if(!GameControl.Instance.isGameOver && timeSinceLastShot >= shootRate){
+            timeSinceLastShot = 0;
+            Vector3 addPos = new Vector3(xFB,yFB,0) + gameObject.transform.position;
+            Instantiate(fireball,addPos,Quaternion.identity);
+        }
     }
 }
