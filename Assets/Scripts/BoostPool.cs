775fe2acf1df9b01cd04d177e524c5c97becc1d9
Roy Leibovitz <royleibo212@gmail.com>
2019-10-19 21:19:27 +0300

    bug fix
    Fix some bugs on pools and add Instrucations to the Main Menu

--- Assets/Scripts/BoostPool.cs
+++ Assets/Scripts/BoostPool.cs
@@ -17,17 +17,24 @@ public class BoostPool : MonoBehaviour
     public GameObject[] boosts;
     private GameObject[] boostsPool1;
     private GameObject[] boostsPool2;
-    private Vector2 objPoolPos = new Vector2(-15f,-25f);
+    private Vector2 objPoolPos = new Vector2(-15f, -25f);
     System.Random rand = new System.Random();
     void Start()
     {
         //create the pool
         boostsPool1 = new GameObject[poolSize];
-        boostsPool2 = new GameObject[poolSize];
-        for (int i=0;i<poolSize;i++){
-            int whichBoost = rand.Next(0,boosts.Length);
-            boostsPool1[i] = (GameObject)Instantiate(boosts[whichBoost],objPoolPos,Quaternion.identity);
-            boostsPool2[i] = (GameObject)Instantiate(boosts[whichBoost], objPoolPos, Quaternion.identity);
+        if (!GameControl.isSingle)
+        {
+            boostsPool2 = new GameObject[poolSize];
+        }
+        for (int i = 0; i < poolSize; i++)
+        {
+            int whichBoost = rand.Next(0, boosts.Length);
+            boostsPool1[i] = (GameObject)Instantiate(boosts[whichBoost], objPoolPos, Quaternion.identity);
+            if (!GameControl.isSingle)
+            {
+                boostsPool2[i] = (GameObject)Instantiate(boosts[whichBoost], objPoolPos, Quaternion.identity);
+            }
         }
     }
 
@@ -35,20 +42,24 @@ public class BoostPool : MonoBehaviour
     {
         //if game isn't over,when spawnRate time passed, spawn new element.
         timeSinceLastSpawn += Time.deltaTime;
-        if(!GameControl.Instance.isGameOver && timeSinceLastSpawn >= spawnRates[currentSpawn]){
+        if (!GameControl.Instance.isGameOver && timeSinceLastSpawn >= spawnRates[currentSpawn])
+        {
             timeSinceLastSpawn = 0;
-            float spawnYPos = Random.Range(boostYMin,boostYMax);
-            float r = Random.Range(1.5f,4.5f); // the addition so that the boost wouldn't collide the columns.
-            boostsPool1[currentBoost].transform.position = new Vector2(spawnXPos + r,spawnYPos-16);
-            boostsPool2[currentBoost].transform.position = new Vector2(spawnXPos + r, spawnYPos-16);
+            float spawnYPos = Random.Range(boostYMin, boostYMax);
+            float r = Random.Range(1.5f, 4.5f); // the addition so that the boost wouldn't collide the columns.
+            boostsPool1[currentBoost].transform.position = new Vector2(spawnXPos + r, spawnYPos);
             //make trigger and visibility enabled again.
             boostsPool1[currentBoost].GetComponent<Collider2D>().enabled = true;
             boostsPool1[currentBoost].GetComponent<Renderer>().enabled = true;
-            boostsPool2[currentBoost].GetComponent<Collider2D>().enabled = true;
-            boostsPool2[currentBoost].GetComponent<Renderer>().enabled = true;
+            if (!GameControl.isSingle)
+            {
+                boostsPool2[currentBoost].transform.position = new Vector2(spawnXPos + r, spawnYPos - 16);
+                boostsPool2[currentBoost].GetComponent<Collider2D>().enabled = true;
+                boostsPool2[currentBoost].GetComponent<Renderer>().enabled = true;
+            }
             currentBoost++;
-            currentSpawn = rand.Next(0,spawnRates.Length);
-            if(currentBoost>=poolSize){currentBoost = 0;}
+            currentSpawn = rand.Next(0, spawnRates.Length);
+            if (currentBoost >= poolSize) { currentBoost = 0; }
         }
     }
 }
--- Assets/Scripts/EnemyPool.cs
+++ Assets/Scripts/EnemyPool.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyPool : MonoBehaviour
 {
     public float[] spawnRates;
-    public int poolSize = 5;
+    public int poolSize = 2;
     private int currentSpawn = 0;
     private float timeSinceLastSpawn = 0;
     private float spawnXPos = 10f;
@@ -15,40 +15,50 @@ public class EnemyPool : MonoBehaviour
     public GameObject enemy;
     private GameObject[] enemyPool1;
     private GameObject[] enemyPool2;
-    private Vector2 enemyPoolPos = new Vector2(-15f,-25f);
+    private Vector2 enemyPoolPos = new Vector2(-15f, -25f);
     System.Random rand = new System.Random();
     void Start()
     {
         //create the pool
         enemyPool1 = new GameObject[poolSize];
-        enemyPool2 = new GameObject[poolSize];
-        for (int i=0;i<poolSize;i++){
-            enemyPool1[i] = (GameObject)Instantiate(enemy,enemyPoolPos,Quaternion.identity);
-            enemyPool2[i] = (GameObject)Instantiate(enemy, enemyPoolPos, Quaternion.identity);
+        for (int i = 0; i < poolSize; i++)
+        {
+            enemyPool1[i] = (GameObject)Instantiate(enemy, enemyPoolPos, Quaternion.identity);
+        }
+        if (!GameControl.isSingle)
+        {
+            enemyPool2 = new GameObject[poolSize];
+            for (int i = 0; i < poolSize; i++)
+            {
+                enemyPool2[i] = (GameObject)Instantiate(enemy, enemyPoolPos, Quaternion.identity);
+            }
         }
     }
-
     // Update is called once per frame
     void Update()
     {
         //if game isn't over,when spawnRate time passed, spawn new element.
         timeSinceLastSpawn += Time.deltaTime;
-        if(!GameControl.Instance.isGameOver && timeSinceLastSpawn >= spawnRates[currentSpawn]){
+        if (!GameControl.Instance.isGameOver && timeSinceLastSpawn >= spawnRates[currentSpawn])
+        {
             timeSinceLastSpawn = 0;
             float spawnYPos = 0;
-            float r = Random.Range(1.75f,4.25f); // the addition so that the boost wouldn't collide the columns.
-            enemyPool1[currentEnemy].transform.position = new Vector2(spawnXPos + r,spawnYPos-16);
-            enemyPool2[currentEnemy].transform.position = new Vector2(spawnXPos + r, spawnYPos-16);
+            float r = Random.Range(1.75f, 4.25f); // the addition so that the boost wouldn't collide the columns.
+            enemyPool1[currentEnemy].transform.position = new Vector2(spawnXPos + r, spawnYPos);
             //make trigger and visibility enabled again.
             enemyPool1[currentEnemy].GetComponent<Collider2D>().enabled = true;
             enemyPool1[currentEnemy].GetComponent<Renderer>().enabled = true;
             enemyPool1[currentEnemy].GetComponent<Enemy>().isDead = false;
-            enemyPool2[currentEnemy].GetComponent<Collider2D>().enabled = true;
-            enemyPool2[currentEnemy].GetComponent<Renderer>().enabled = true;
-            enemyPool2[currentEnemy].GetComponent<Enemy>().isDead = false;
+            if (!GameControl.isSingle)
+            {
+                enemyPool2[currentEnemy].transform.position = new Vector2(spawnXPos + r, spawnYPos - 16);
+                enemyPool2[currentEnemy].GetComponent<Collider2D>().enabled = true;
+                enemyPool2[currentEnemy].GetComponent<Renderer>().enabled = true;
+                enemyPool2[currentEnemy].GetComponent<Enemy>().isDead = false;
+            }
             currentEnemy++;
-            currentSpawn = rand.Next(0,spawnRates.Length);
-            if(currentEnemy>=poolSize){currentEnemy = 0;}
+            currentSpawn = rand.Next(0, spawnRates.Length);
+            if (currentEnemy >= poolSize) { currentEnemy = 0; }
         }
     }
 }
